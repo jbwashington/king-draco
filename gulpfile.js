24652e82f4be6fc551521bde9ba56bef244f51cc
James Washington <jbwashington@gmail.com>
2017-03-29 10:43:38 -0400

    added lodash and bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,66 @@
-
-// ES6 style Promises support for gulp-autoprefixer
 require('es6-promise').polyfill();
 
-// declare variables for gulp tasks
-var gulp = require('gulp');
-var sass  = require('gulp-sass');
-var autoprefixer  = require('gulp-autoprefixer');
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    rtlcss        = require('gulp-rtlcss'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    plumber       = require('gulp-plumber'),
+    gutil         = require('gulp-util'),
+    rename        = require('gulp-rename'),
+    concat        = require('gulp-concat'),
+    jshint        = require('gulp-jshint'),
+    uglify        = require('gulp-uglify'),
+    imagemin      = require('gulp-imagemin'),
+    browserSync   = require('browser-sync').create(),
+    reload        = browserSync.reload;
 
-// sass compiling task to relative root dir
+var onError = function( err ) {
+  console.log('An error occurred:', gutil.colors.magenta(err.message));
+  gutil.beep();
+  this.emit('end');
+};
+
+// Sass
 gulp.task('sass', function() {
-    return gulp.src('.sass/**/*.scss')
-      .pipe(sass())
-      .pipe(autoprefixer())
-      .pipe(gulp.dest('./'))
-  });
+  return gulp.src('./sass/**/*.scss')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./'))
 
-// default task
-gulp.task('default', ['sass']);
+  .pipe(rtlcss())                     // Convert to RTL
+  .pipe(rename({ basename: 'rtl' }))  // Rename to rtl.css
+  .pipe(gulp.dest('./'));             // Output RTL stylesheets (rtl.css)
+});
+
+// JavaScript
+gulp.task('js', function() {
+  return gulp.src(['./js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(concat('app.js'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(uglify())
+  .pipe(gulp.dest('./js'));
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('./images/src/*')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(imagemin({ optimizationLevel: 7, progressive: true }))
+  .pipe(gulp.dest('./images/dist'));
+});
+
+// Watch
+gulp.task('watch', function() {
+  browserSync.init({
+    files: ['./**/*.php'],
+    proxy: 'http://localhost:8888/',
+  });
+  gulp.watch('./sass/**/*.scss', ['sass', reload]);
+  gulp.watch('./js/*.js', ['js', reload]);
+  gulp.watch('images/src/*', ['images', reload]);
+});
+
+gulp.task('default', ['sass', 'js', 'images', 'watch']);
