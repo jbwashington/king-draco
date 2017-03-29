3f33d25a01bc50c9a50ff692240179241271ed8b
James Washington <jbwashington@gmail.com>
2017-03-29 10:08:18 -0400

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,18 @@
 // ES6 style Promises support for gulp-autoprefixer
 require('es6-promise').polyfill();
 
+// declare variables for gulp tasks
 var gulp = require('gulp');
+var sass  = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
+
+// sass compiling task to relative root dir
+gulp.task('sass', function() {
+    return gulp.src('.sass/**/*.scss')
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('./'))
+  });
 
 // default task
-gulp.task('default');
+gulp.task('default', ['sass']);
